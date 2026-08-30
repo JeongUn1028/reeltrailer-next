62d51b39bba8fd776574bbad38e4b9b3af14aa4c
JeongUn1028 <wjddns363@naver.com>
2026-08-31 08:30:33 +0900

    feat: @vercel/speed-insights 패키지 추가 및 레이아웃에 통합

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/header/header";
 import Providers from "./provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -34,6 +35,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="min-h-full flex flex-col">
+        <SpeedInsights />
         <Providers>
           <Header />
           {children}
